8a509bc7d9194bac43d5a259b8081f3a44b9dcc7
Rodrigo <rodrigo@infoservsoftware.com.br>
2019-08-30 13:43:52 -0300

    chore: Session route validation

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,11 +1,18 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const Yup = require('yup');
 
 const create = async (User, req, res) => {
     const { email, password } = req.body;
 
-    const user = await User.findOne({ 
-        where: { email }
+    const schema = Yup.object().shape({
+        email: Yup.string().email().required(),
+        password: Yup.string().required(),
     })
+    if (!(await schema.isValid(req.body))) {
+        return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const user = await User.findOne({ where: { email } })
 
     if (!user) {
         return res.status(401).json({ error: 'Could not find your account.' })
@@ -16,6 +23,7 @@ const create = async (User, req, res) => {
     }
 
     const { id, name } = user;
+    
     return res.json({ 
         token: jwt.sign({ id, name, email }, '3d18af19790ccfc67fe83bcec716524c', { expiresIn: '7d' }) 
     })
